3b0af4f88cc39c76496a850b0dec91362cebaa54
Ankit Mishra <59928958+friendlyNeighbourhoodFool@users.noreply.github.com>
2021-06-10 22:26:43 +0530

    Add files via upload

---
+++ A_Cheap_Travel.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define rep(i, j, n) for (int i = j; i < n; i++)
+#define pre(i, j, n) for (int i = j; i >= n; i--)
+#define debug(a...)        \
+    cout << #a << ": ";    \
+    for (auto it : a)      \
+        cout << it << " "; \
+    cout << endl;
+#define deb(a) cout << #a << " " << a << endl;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const string no = "NO", yes = "YES";
+const ll inf = 1e18;
+const int mod = 1e9 + 7;
+const ld pi = acosl(-1.0);
+
+void solve()
+{
+    int n,m,a,b;
+    cin>>n>>m>>a>>b;
+    ld per=b/(ld)m;
+    if(per>a){
+        cout<<a*n;
+        return;
+    }
+    int cost = 0;
+    cost+=(n/m)*b;
+    int covered = (n/m)*m;
+    n-=covered;
+    ld cost1=a*n;
+    cost+=min(cost1,(ld)b);
+    cout<<cost<<endl;
+}
+
+int main(){
+	//	freopen("input.txt", "r", stdin);
+	//	freopen("output.txt", "w", stdout);
+	fast;
+	int t=1;
+	//cin>>t;
+	while(t--)
+		solve();
+}
